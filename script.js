35f8e82c07f2ccee81a9b1ba8a76cba15394151a
Lily <mengl415@newschool.edu>
2021-02-19 01:19:02 -0600

    added assets and 1 media query

---
+++ script.js
@@ -0,0 +1,23 @@
+/*
+
+var bunny = document.getElementById("bunny");
+var bear = document.getElementById("bear");
+var cat = document.getElementById("cat");
+var controller = document.getElementById("controller");
+var flower = document.getElementById("flower");
+var giraffe = document.getElementById("giraffe");
+var pea = document.getElementById("pea");
+var pencils = document.getElementById("pencils");
+
+bunny.addEventListener("mouseenter", BunnyOpacity);
+bunny.addEventListener("mouseleave", BunnyBack);
+
+function BunnyOpacity(){
+    bunny.style.opacity = "1";
+}
+
+function BunnyBack(){
+    bunny.style.opacity = "0.5";
+}
+
+*/
